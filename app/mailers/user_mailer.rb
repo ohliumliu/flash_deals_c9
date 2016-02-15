a6bcee2f7f3e786dd51300df862f6590d3c79a2d
Yu Liu <yul.liuyu@gmail.com>
2016-02-15 06:17:44 +0000

    send email to users with alerts

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,10 @@ class UserMailer < ActionMailer::Base
     @url = 'http://localhost:3000'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+  
+  def alert_email(user)
+    @user = user
+    @url = 'http://localhost:3000'
+    mail(to: @user.email, subject: 'Here is your alerts')
+  end
 end
